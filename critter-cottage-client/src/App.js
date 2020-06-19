5ae89375442aed45dad0322f23de25c7402b9694
ctdupuis <ctdupuis7@gmail.com>
2020-06-18 22:25:16 -0500

    setup initial Route path

--- critter-cottage-client/src/App.js
+++ critter-cottage-client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-     <Home />
-    </div>
+    <Router>
+      <div>
+        <Route path='/' component={Home} />
+      </div>
+    </Router>
   );
 }
 
