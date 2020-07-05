18de97f43f3461e87bc3177b76ea1693f63ffaa1
ctdupuis <ctdupuis7@gmail.com>
2020-07-04 21:15:36 -0500

    fix export statement in Request.js

--- critter-cottage-client/src/components/requests/Request.js
+++ critter-cottage-client/src/components/requests/Request.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default Request = ({ request }) => {
+const Request = ({ request }) => {
     const { id, status } = request
     return(
         <div>
@@ -8,3 +8,5 @@ export default Request = ({ request }) => {
         </div>
     )
 }
+
+export default Request;
